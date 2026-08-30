fa46cac19ef34d2aca2096d94cf32aae9f7f2a63
ngwebwork <websitedeveloperng1@gmail.com>
2026-08-30 08:15:39 -1200

    Apply editor formatting to favoriteService.js
    Cosmetic only (quote style, line wrapping) - no behavior change.

--- client/src/services/favoriteService.js
+++ client/src/services/favoriteService.js
@@ -1,9 +1,11 @@
-import api from './api';
+import api from "./api";
 
-const getFavorites = () => api.get('/favorites').then((res) => res.data);
+const getFavorites = () => api.get("/favorites").then((res) => res.data);
 
-const addFavorite = (propertyId) => api.post(`/favorites/${propertyId}`).then((res) => res.data);
+const addFavorite = (propertyId) =>
+  api.post(`/favorites/${propertyId}`).then((res) => res.data);
 
-const removeFavorite = (propertyId) => api.delete(`/favorites/${propertyId}`).then((res) => res.data);
+const removeFavorite = (propertyId) =>
+  api.delete(`/favorites/${propertyId}`).then((res) => res.data);
 
 export default { getFavorites, addFavorite, removeFavorite };
